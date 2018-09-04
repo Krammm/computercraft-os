2fac4b3ab2e9f162cbc37a2de96e0846a30f98d0
Krammm <42967229+Krammm@users.noreply.github.com>
2018-09-04 15:57:47 +0200

    Update login.lua
    Re-coded

--- programs/login.lua
+++ programs/login.lua
@@ -1,2 +1,33 @@
+os.loadAPI("computercraft-os/apis/sha256.lua")
+
 local passfile = fs.open("computercraft-os/user/password.txt", "r")
 local password = passfile.readAll()
+local loggedIn = false
+
+term.clear()
+term.setCursorPos(1, 1)
+
+term.setTextColor(colors.orange)
+print("Insert your password. ")
+
+while loggedIn == false do
+  
+  term.setTextColor(colors.white)
+  local passwordtry = read()
+
+  if sha256.sha256(passwordtry) == password then
+  
+    loggedIn = true
+    term.setTextColor(colors.green)
+    print("Access Granted.")
+    
+  else
+  
+    term.setTextColor(colors.red)  
+    print("Wrong Password. Rerty")
+    
+  end
+
+end
+
+print("You can change your password with the login-config command.")
