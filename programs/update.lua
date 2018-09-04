97066f983912b98649154a07632c263cab8e36f7
Krammm <42967229+Krammm@users.noreply.github.com>
2018-09-04 17:13:04 +0200

    Update update.lua

--- programs/update.lua
+++ programs/update.lua
@@ -1,16 +1,18 @@
 os.loadAPI("computercraft-os/apis/default.lua")
 
-setTextColor(default.mod_color)
+term.setTextColor(default.mod_color)
 
 print("Removing old files... ")
 fs.move("computercraft-os/user", "")
-fs.remove("computercraft-os/")
+fs.delete("computercraft-os/")
 
 print("Creating new files... ")
 fs.makeDir("computercraft-os/")
 shell.run("cd computercraft-os")
 shell.run("gitget Krammm computercraft-os")
 fs.move("computercraft-os/startup.lua", "")
+fs.delete("computercraft-os/user")
+fs.move("user", "computercraft-os")
 
 setTextColor(default.success_color)
 print("Successfully Updated.")
