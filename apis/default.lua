9a8e6133ac01b52f8177ce32fa72a49e83f07399
Krammm <42967229+Krammm@users.noreply.github.com>
2018-09-04 17:10:16 +0200

    Update default.lua

--- apis/default.lua
+++ apis/default.lua
@@ -3,3 +3,15 @@ default_color = colors.white
 error_color = colors.red
 
 
+function clear() 
+  
+  term.clear()
+  term.setCursorPos(1, 1)
+  term.setTextColor(default.mod_color)
+  print("> ")
+  term.setCursorPos(3, 1)
+  term.setTextColor(default.default_color)
+  
+end
+
+
