11316cd6cc7a865b7cfb22a9f9fcad8ebe7bbce5
Krammm <42967229+Krammm@users.noreply.github.com>
2018-09-04 16:21:45 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,36 @@
 -- This file handles user-inserted commands.
+
+default_color = colors.white
+mod_color = colors.cyan
+error_color = colors.red
+
+local function clear() 
+  
+  term.clear()
+  term.setCursorPos(1, 1)
+  term.setTextColor(mod_color)
+  print("> ")
+  term.setCursorPos(1, 3)
+  term.setTextColor(default_color)
+  
+end
+
+while true do
+  
+  clear()
+  local command = read()
+  
+  local success = fs.exists("computercraft-os/programs/" .. command .. ".lua")
+  
+  if success then
+    
+      shell.run("computercraft-os/programs/" .. command .. ".lua")
+    
+  else 
+    
+    term.setTextColor(error_color)
+    print("Error. '" .. command .. "' command not found.")
+    
+  end
+  
+end
