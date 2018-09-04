d5caf73c775324398710049c0d962a9c7238c0a7
Krammm <42967229+Krammm@users.noreply.github.com>
2018-09-04 17:09:48 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,10 @@
 -- This file handles user-inserted commands.
 os.loadAPI("computercraft-os/apis/default.lua")
 
-local function clear() 
-  
-  term.clear()
-  term.setCursorPos(1, 1)
-  term.setTextColor(default.mod_color)
-  print("> ")
-  term.setCursorPos(3, 1)
-  term.setTextColor(default.default_color)
-  
-end
+default.clear()
 
 while true do
   
-  clear()
   local command = read()
   
   local success = fs.exists("computercraft-os/programs/" .. command .. ".lua")
